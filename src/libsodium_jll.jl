1240a8b9e5b3bb15b07ee0104eb3cdb8fe1bf7f2
jlbuild <juliabuildbot@gmail.com>
2020-04-03 02:13:46 +0000

    libsodium_jll build 1.0.18+1

--- src/libsodium_jll.jl
+++ src/libsodium_jll.jl
@@ -34,7 +34,7 @@ artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("a91
 platforms = [Pkg.Artifacts.unpack_platform(e, "libsodium", artifacts_toml) for e in artifacts["libsodium"]]
 
 # Filter platforms based on what wrappers we've generated on-disk
-platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+filter!(p -> isfile(joinpath(@__DIR__, "wrappers", replace(triplet(p), "arm-" => "armv7l-") * ".jl")), platforms)
 
 # From the available options, choose the best platform
 best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
